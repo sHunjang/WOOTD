4ce5b4893c68a82ac4329f6dd1b216ab2a0f07c1
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-04-21 20:37:44 +0900

    feat : Fix model_predict.py Code  Annotation

--- model_predict.py
+++ model_predict.py
@@ -1,11 +1,11 @@
 from ultralytics import YOLO
 
 
-# Load pretrained model
+# Load pretrained model Path
 model = YOLO('/Users/seunghunjang/Desktop/WOOTD/results/BGR_X/Train_Models/train5/weights/best.pt')
 
 
-# Load Pretrained BGR Model
+# Load Pretrained BGR Model Path
 model_BGR = YOLO('/Users/seunghunjang/Desktop/WOOTD/results/BGR/Train_BGR_Models/train5/weights/best.pt')
 
 
@@ -18,17 +18,17 @@ predic_BGR_img_path = '/Users/seunghunjang/Desktop/WOOTD/TestDataset/BGR'
 top_bottom_path = '/Users/seunghunjang/Desktop/WOOTD/Top_Bottom_Combination'
 
 
-# Save Dir
+# Save Dir Path
 result_path = '/Users/seunghunjang/Desktop/WOOTD/results/BGR_X/Predict'
 result_bgr_path = '/Users/seunghunjang/Desktop/WOOTD/results/BGR/BGR_Predict'
 result_combination_path = '/Users/seunghunjang/Desktop/WOOTD/results/Combination'
 
 
-# Model Test
+# None Remove BackGround Model Test
 result = model.predict(source=predic_img_path, save=True, save_txt=True, project=result_path)
 
-# Model Test
+# Remove BackGround Model Test
 result_bgr = model_BGR.predict(source=predic_BGR_img_path, save=True, save_txt=True, project=result_bgr_path)
 
-# Model Test
+# Model Test Only Top/Bottom Clothes Combination
 result_combination = model_BGR.predict(source=top_bottom_path, save=True, save_txt=True, project=result_combination_path)
