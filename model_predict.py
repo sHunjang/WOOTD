8f51f8912a7e78b8985b590450988a8a59e978d2
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-05-17 16:54:22 +0900

    feat : Change Wannabe Style Class Images & Add Amekaji Class & Predict Images

--- model_predict.py
+++ model_predict.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 
 
 # Load Pretrained BGR Model Path
-model = YOLO('')
+model = YOLO('results/BGR/Train_BGR_Models/train14/weights/best.pt')
 
 
 # Predict Image Path
@@ -19,4 +19,4 @@ test_path = 'test_Combination'
 
 
 # Model Test Only Top/Bottom Clothes Combination
-result_combination = model.predict(source=test_path, save=True, save_txt=True, project='test')
+result_combination = model.predict(source=top_bottom_path, save=True, save_txt=True, project='results/BGR/BGR_Predict')
--- model_train.py
+++ model_train.py
@@ -7,10 +7,13 @@ model_BGR = YOLO('yolov8n-cls_BGR.pt')
 # Image Path
 img_bgr_path = '/Users/seunghunjang/Desktop/WOOTD/Dataset_BGR'
 
+# Other Wannabe Style Dataset
+otherStyle_Dataset = 'OtherStyle_Dataset'
+
 
 # Model Save Path
 save_model_path = 'results/BGR/Train_BGR_Models'
 
 
 # BGR Model Train
-result_bgt = model_BGR.train(data=img_bgr_path, epochs=10, cache=True, imgsz=480, project=save_model_path)
+result_bgt = model_BGR.train(data=otherStyle_Dataset, epochs=50, cache=True, imgsz=480, project=save_model_path)
