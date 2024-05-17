f56980d62d5ff81cc405b71159f87f8583bd2810
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-05-17 15:13:39 +0900

    feat : Folder Renewal

--- model_predict.py
+++ model_predict.py
@@ -1,41 +1,22 @@
 from ultralytics import YOLO
 
 
-# Load pretrained model Path
-#model = YOLO('/Users/seunghunjang/Desktop/WOOTD/20240430/Train_Model/train/weights/best.pt')
-
 
 # Load Pretrained BGR Model Path
-model_BGR_bestPT = YOLO('20240514/Train_Model/train2/weights/best.pt')
-#model_BGR_lastPT = YOLO('20240514/Train_Model/train6/weights/last.pt')
+model = YOLO('')
 
 
 # Predict Image Path
-predic_BGR_img_path = '/Users/seunghunjang/Desktop/WOOTD/TestDataset'
+predic_BGR_img_path = 'TestDataset'
 
 
 # Top Bottom Combination Path
-top_bottom_path = '/Users/seunghunjang/Desktop/WOOTD/Top_Bottom_Combination'
+top_bottom_path = 'Top_Bottom_Combination'
 
-# 20240430~ Test
-test_path_best = '/Users/seunghunjang/Desktop/WOOTD/20240514/Predict_Results/bestPT'
-test_path_last = '/Users/seunghunjang/Desktop/WOOTD/20240514/Predict_Results/lastPT'
-
-# Save Dir Path
-result_path = '/Users/seunghunjang/Desktop/WOOTD/results/BGR_X/Predict'
-result_bgr_path = '/Users/seunghunjang/Desktop/WOOTD/results/BGR/BGR_Predict'
-result_combination_path = '/Users/seunghunjang/Desktop/WOOTD/results/Combination'
 
 # Test Combination
 test_path = 'test_Combination'
 
-# None Remove BackGround Model Test
-#result = model.predict(source=predic_img_path, save=True, save_txt=True, project=result_path)
-
-# Remove BackGround Model Test
-#result_bgr = model_BGR.predict(source=predic_BGR_img_path, save=True, save_txt=True, project=result_bgr_path)
 
 # Model Test Only Top/Bottom Clothes Combination
-result_combination = model_BGR_bestPT.predict(source=test_path, save=True, save_txt=True, project='test')
-
-#result_combination = model_BGR_lastPT.predict(source=top_bottom_path, save=True, save_txt=True, project=test_path_last)
+result_combination = model.predict(source=test_path, save=True, save_txt=True, project='test')
--- model_train.py
+++ model_train.py
@@ -1,26 +1,16 @@
 from ultralytics import YOLO
 
 # Model Load
-model = YOLO('yolov8n-cls.pt')
 model_BGR = YOLO('yolov8n-cls_BGR.pt')
 
 
 # Image Path
-img_path = '/Users/seunghunjang/Desktop/WOOTD/Dataset'
 img_bgr_path = '/Users/seunghunjang/Desktop/WOOTD/Dataset_BGR'
 
-# Result Path
-save_model_path = '/Users/seunghunjang/Desktop/WOOTD/results/BGR_X/Train_Models'
 
-# BGR Model Save Path
-save_bgr_model_path = '/Users/seunghunjang/Desktop/WOOTD/results/BGR/Train_BGR_Models'
+# Model Save Path
+save_model_path = 'results/BGR/Train_BGR_Models'
 
-# 20240514 Test
-Model_path = '/Users/seunghunjang/Desktop/WOOTD/20240514/Train_Model'
-
-
-# Model Train
-#result = model.train(data=img_path, epochs=10, cache=True, imgsz=480, project=save_model_path)
 
 # BGR Model Train
-result_bgt = model_BGR.train(data=img_bgr_path, epochs=10, cache=True, imgsz=480, project=Model_path)
+result_bgt = model_BGR.train(data=img_bgr_path, epochs=10, cache=True, imgsz=480, project=save_model_path)
